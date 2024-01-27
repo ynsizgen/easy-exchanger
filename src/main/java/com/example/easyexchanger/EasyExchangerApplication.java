fb9aa222ed189e49c91ca6ac1d554af295367258
Yunus İzgen ÜN <yunusizgen@gmail.com>
2024-01-27 16:16:25 +0300

    initial commit

---
+++ src/main/java/com/example/easyexchanger/EasyExchangerApplication.java
@@ -0,0 +1,13 @@
+package com.example.easyexchanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyExchangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyExchangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/easyexchanger/EasyExchangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easyexchanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyExchangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
