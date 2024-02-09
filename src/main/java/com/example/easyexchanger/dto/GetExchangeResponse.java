0c08f9c5760b157ae6440f320550f611a0b4f2d9
Yunus İzgen ÜN <yunusizgen@gmail.com>
2024-02-09 17:28:32 +0300

    new feature

--- src/main/java/com/example/easyexchanger/dto/GetExchangeResponse.java
+++ src/main/java/com/example/easyexchanger/dto/GetExchangeResponse.java
@@ -1,23 +1,34 @@
 package com.example.easyexchanger.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Map;
 
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record GetExchangeResponse(
         String result,
-        String documentation,
-        @JsonProperty("terms_of_use")
-        String termsOfUse,
-        @JsonProperty("time_last_update_unix")
-        String timeLastUpdateUnix,
+
+        //String documentation,
+
+        //@JsonProperty("terms_of_use")
+        //String termsOfUse,
+
+        //@JsonProperty("time_last_update_unix")
+        //String timeLastUpdateUnix,
+
         @JsonProperty("time_last_update_utc")
         String timeLastUpdateUtc,
-        @JsonProperty("time_next_update_unix")
-        String timeNextUpdateUnix,
+
         @JsonProperty("time_next_update_utc")
         String timeNextUpdateUtc,
+
+        //@JsonProperty("time_next_update_unix")
+        //String timeNextUpdateUnix,
+
         @JsonProperty("base_code")
         String baseCode,
+
         @JsonProperty("conversion_rates")
         Map<String,Double> conversionRates
 
